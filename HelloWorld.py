39108d119f16e6b1f8e90882bc49271abd908bd1
Lie-CircleCI <lie-circleci@kismetworldwide.com>
2016-02-08 16:20:05 -0800

    First Commit, simple Hello, World to the command line...

---
+++ HelloWorld.py
@@ -0,0 +1,10 @@
+# Hello World test for Mahmood @ CircleCI
+# 
+# Requirments: build a python web server that takes to GET arguments as integers and returns the result as JSON
+#
+# Goals:  simple, quick, just make it work
+#
+
+#Step 1:  make Hell World print to STDOUT:
+
+print "Hello, World!"
